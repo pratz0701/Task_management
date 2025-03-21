a3a9a74fdcdb96f9a37e070873aa0925bb132fab
pratz0701 <pratishthasingh0701@gmail.com>
2025-03-21 16:18:11 +0530

    credentials attirbute removed

--- my-react-app/src/useHooks/useCrudOperations.jsx
+++ my-react-app/src/useHooks/useCrudOperations.jsx
@@ -1,21 +1,22 @@
 import { useState, useCallback } from 'react';
 
-const useCrudOperations = ({ url, method }) => {
+const useCrudOperations = ({ url, method}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  console.log("url and method",url,method)
 
   const executeRequest = useCallback(async (requestData) => {
-    const body = requestData?.requestData
-    console.log("body os",body)
+    const body = requestData?.requestData;
     setLoading(true);
     setError(null);
     try {
       const response = await fetch(url, {
         method,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+          'Content-Type': 'application/json' ,
+        },
         body: body ? JSON.stringify(body) : null,
+        // credentials:'include',
       });
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
